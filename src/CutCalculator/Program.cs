33a755257fedfb60cd8a946860159e9ad68aa6c3
tejver <tejver@users.noreply.github.com>
2026-08-14 13:34:21 +0100

    Explicit types

--- src/CutCalculator/Program.cs
+++ src/CutCalculator/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CutCalculator;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
